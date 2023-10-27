e488ce430cd86ffb8b2e3e80f6b449564f4aff6d
Fathan Mubarok <50894040+falonez@users.noreply.github.com>
2023-10-27 10:03:16 +0700

    fix vite config pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
-const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
-  manifest:{
-    name:"Tuangkeun",
-    short_name:"Tuangkeun",
-    description:"Tuangkeun App",
-    icons:[{
-      src: '/android-chrome-192x192.png',
-      sizes:'192x192',
-      type:'image/png',
-      purpose:'favicon'
-    },
-    {
-      src:'/android-chrome-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'favicon'
-    },
-    {
-      src: '/apple-touch-icon.png',
-      sizes:'180x180',
-      type:'image/png',
-      purpose:'apple touch icon',
-    },
-    {
-      src: '/maskable_icon.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable',
-    }
-  ],
-  theme_color:'#171717',
-  background_color:'#f0e7db',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
-  }
+import { VitePWA, VitePWAOptions  } from "vite-plugin-pwa";
+const manifestForPlugIn : Partial <VitePWAOptions> = {
+  registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+	manifest: {
+		name: "Tuangkeun Kuy",
+		short_name: "Tuangkeun Kuy",
+		description: "An app that can show weather forecast for your city.",
+		icons: [
+			{
+				src: "/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+			{
+				src: "/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "apple touch icon",
+			},
+			{
+				src: "/maskable_icon.png",
+				sizes: "225x225",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+		],
+		theme_color: "#171717",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
 }
 
 // https://vitejs.dev/config/
